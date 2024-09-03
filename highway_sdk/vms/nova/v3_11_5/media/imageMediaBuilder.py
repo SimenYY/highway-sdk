ecd9b65112d90ce0ce149da5bb30228fae35ba98
AdzLovelace <heyinyu.sensi@foxmail.com>
2024-09-03 16:22:25 +0800

    commit message:``` 新增ImageMedia和ImageMediaBuilder类
    添加新的ImageMedia类，用于处理图像媒体对象，以及ImageMediaBuilder类，提供构建ImageMedia对象的工厂方法。这些类继承自BaseMedia和BaseMediaBuilder，分别实现了与图像媒体相关的特定功能和属性，如文件路径和闪烁效果。
    ```

---
+++ highway_sdk/vms/nova/v3_11_5/media/imageMediaBuilder.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+:FileName: imageMediaBuilder.py
+:Project:
+:Brand:
+:Version:
+:Description: 
+:Author: He YinYu
+:Link:
+:Time: 2024/9/3 15:35
+"""
+from baseMediaBuilder import BaseMediaBuilder
+from imageMedia import ImageMedia
+
+
+class ImageMediaBuilder(BaseMediaBuilder):
+
+    def __init__(self):
+        super().__init__()
+        # 图片文件路径名
+        self._file_path: str = ''
+        # 闪烁
+        self._flash: str = '0'
+
+    def build(self):
+        return ImageMedia(**self.to_dict())
+
+    @property
+    def file_path(self) -> str:
+        return self._file_path
+
+    @file_path.setter
+    def file_path(self, file_path: str) -> None:
+        self._file_path = file_path
+
+    @property
+    def flash(self) -> str:
+        return self._flash
+
+    @flash.setter
+    def flash(self, flash: str) -> None:
+        self._flash = flash
