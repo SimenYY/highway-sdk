d98ba8be9857f5e232e23064bddd49c217774ea2
AdzLovelace <heyinyu.sensi@foxmail.com>
2024-09-20 09:17:23 +0800

    feat: 添加异步客户端支持和进行客户端类的代码优化
    - 引入异步客户端类（AsyncClient），支持异步连接和通信。
    - 对同步客户端类（Client）进行代码优化，改进连接和关闭方法。
    - 所有网络地址现在使用`host`而非`ip`命名，以提高通用性。
    - 移除冗余的socket创建方法，通过构造函数直接实例化socket。
    - 异步客户端使用StreamReader和StreamWriter进行异步IO操作。
    - 引入`asyncio`库支持异步操作。
    
    BREAKING CHANGE: 已修改客户端初始化和连接方法，这可能影响现有调用代码。
    ```

--- highway_sdk/core/client.py
+++ highway_sdk/core/client.py
@@ -18,34 +18,35 @@ from highway_sdk.core.validators import (
     validate_ipv4_address,
     validate_port,
 )
+import asyncio
+from asyncio import StreamReader, StreamWriter
 
 
 class Client:
-    # TODO 实现异步连接
     # 响应超时时间
     rsp_timeout: int = 3
     # 接受字节流大小单位
     buf_size: int = 1024
 
-    def __init__(self, ip: str, port: int):
+    def __init__(self, host: str, port: int):
         """
         不合法的通信地址要让实例一开始就不成立
         """
-        validate_ipv4_address(ip)
+        validate_ipv4_address(host)
         validate_port(port)
 
-        self.ip: str = ip
+        self.host: str = host
         self.port: int = port
         self._sock: Optional[socket.socket] = None
 
     def __enter__(self):
-        self.make_connection()
+        self.connect()
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        self.close_connection()
+        self.close()
         if exc_type is not None:
-            logger.error(f'{self.log_prefix()} {exc_type} {exc_val} {exc_tb}')
+            logger.error(f'{self.log_addr()} {exc_type} {exc_val} {exc_tb}')
         # 抑制异常
         return True
 
@@ -57,22 +58,63 @@ class Client:
     def sock(self, sock: socket.socket):
         self._sock = sock
 
-    def make_connection(self):
+    def connect(self):
         if self._sock is not None:
             return
 
         try:
             self._sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             self._sock.settimeout(self.rsp_timeout)
-            self._sock.connect((self.ip, self.port))
+            self._sock.connect((self.host, self.port))
         except (TimeoutError, ConnectionRefusedError, Exception) as e:
-            logger.error(f'{self.log_prefix()} {e}')
-            self.close_connection()
+            logger.error(f'{self.log_addr()} {e}')
+            self.close()
 
-    def close_connection(self):
+    def close(self):
         if self._sock is not None:
             self._sock.close()
             self._sock = None
 
-    def log_prefix(self) -> str:
-        return f'{self.ip}:{self.port}'
+    def log_addr(self) -> str:
+        return f'{self.host}:{self.port}'
+
+
+class AsyncClient:
+    # 接受字节流大小单位
+    buf_size: int = 1024
+
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.reader: StreamReader | None = None
+        self.writer: StreamWriter | None = None
+
+    async def connect(self) -> bool:
+        ret = True
+        try:
+            self.reader, self.writer = await asyncio.open_connection(self.host, self.port)
+        except ConnectionRefusedError as e:
+            logger.error(f'{self.log_addr()} {e}')
+            await self.close()
+            ret = False
+        return ret
+
+    async def send(self, msg: bytes):
+        if self.writer is None:
+            raise Exception("Not connected to server")
+        self.writer.write(msg)
+        await self.writer.drain()
+
+    async def recv(self) -> bytes:
+        if self.reader is None:
+            raise Exception("Not connected to server")
+        data = await self.reader.read(self.buf_size)
+        return data
+
+    async def close(self):
+        if self.writer is not None:
+            self.writer.close()
+            await self.writer.wait_closed()
+
+    def log_addr(self) -> str:
+        return f'{self.host}:{self.port}'
