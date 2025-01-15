be2dfbfc67cadcfff89674ed112c57d38a1fbf42
AdzLovelace <heyinyu.sensi@foxmail.com>
2025-01-15 15:52:55 +0800

    feat(highway_sdk): 更新 __init__.py 以支持多个 VMS 系统
    - 引入了多个 VMS 系统的模块，包括 Nova、SanSi 和 DianMing
    - 移除了原有的日志相关代码
    - 为每个 VMS 系统引入了相应的 internet、Protocol 和 Client 模块

--- highway_sdk/__init__.py
+++ highway_sdk/__init__.py
@@ -12,11 +12,16 @@
 :Department: 公路机电工程技术中心
 :Copyright: ©1993-2023 浙江中控信息产业股份有限公司
 """
-import logging
-from highway_sdk.core.log.handlers import ColoredStreamHandler
+from highway_sdk.vms.nova.v3_11_5 import internet as nova_internet
+from highway_sdk.vms.nova.v3_11_5.internet.protocol import Protocol as NovaProtocol
+from highway_sdk.vms.nova.v3_11_5.internet.novaClient import NovaClient as NovaClient
+from highway_sdk.vms.SanSi.v4_21_0 import internet as sansi_internet
+from highway_sdk.vms.SanSi.v4_21_0.internet.protocol import Protocol as SanSiProtocol
+from highway_sdk.vms.SanSi.v4_21_0.internet.sanSiClient import SanSiClient as SanSiClient
+from highway_sdk.vms.DianMing.v2_3_0 import internet as dianming_internet
+from highway_sdk.vms.DianMing.v2_3_0.internet.protocol import Protocol as DianMingProtocol
+from highway_sdk.vms.DianMing.v2_3_0.internet.dmClient import DmClient as DianMingClient
 
-logger = logging.getLogger(__name__)
 
 
-def get_lib_name():
-    return __name__
+
