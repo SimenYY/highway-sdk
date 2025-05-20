a21da084c69682b0dfdc9580ed6c7c5098f50493
AdzLovelace <heyinyu.sensi@foxmail.com>
2025-05-20 16:02:25 +0800

    feat(utils): 添加新的 logger_config模块并弃用旧的日志配置类
    - 新增 logger_config.py 文件，实现更灵活的日志配置功能
    - 弃用 BaseLoggerConfig、DriverLoggerConfig 和 ApiLoggerConfig 类
    - 更新 get_driver_loger 函数，使用新的日志配置模块

--- highway_sdk/core/logx.py
+++ highway_sdk/core/logx.py
@@ -19,85 +19,10 @@ from highway_sdk.core.log.handlers import InterceptHandler
 from deprecated import deprecated
 
 
-@deprecated(reason="脱裤子放屁，多此一举的一个类， 用【get_driver_loger】代替", version='1.19.1')
-class BaseLoggerConfig:
-    def __init__(self):
-        self._logger = logger
-        self._logger.remove()
-
-    @property
-    def logger(self):
-        return self._logger
-
-
-@deprecated(reason="脱裤子放屁，多此一举的一个类， 用【get_driver_loger】代替", version='1.19.1')
-class DriverLoggerConfig(BaseLoggerConfig):
-    """
-    用于驱动脚本的日志配置
-
-    默认的使用方式
-    logger = DriverLoggerConfig().logger
-    """
-
-    def __init__(
-            self,
-            name: str = 'none',
-            brand: str = 'none',
-            level: str = 'DEBUG',
-            rotation: str = '1 day',
-            retention: str = '7 days',
-            compression: str = 'zip',
-            enqueue: bool = True,
-            file: bool = True,
-            console: bool = True
-    ):
-        super().__init__()
-
-        if file:
-            self._logger.add(
-                f'logs/{name}/' + f'{brand}' + '_{time: YYYY-MM-DD}.log',
-                level=level,
-                rotation=rotation,
-                retention=retention,
-                compression=compression,
-                enqueue=enqueue
-            )
-        if console:
-            self._logger.add(
-                sys.stdout,
-                level=level,
-            )
-
-
-@deprecated(reason="脱裤子放屁，多此一举的一个类， 用【get_driver_loger】代替", version='1.19.1')
-class ApiLoggerConfig(BaseLoggerConfig):
-    """
-    Api 服务日志配置
-
-    默认的使用方式
-    logger = ApiLoggerConfig().logger
-    """
-
-    def __init__(
-            self,
-            log_file: str = 'api.log',
-            level: str = 'INFO',
-            rotation: str = '1 day',
-            retention: str = '7 days',
-            compression: str = True,
-            enqueue: bool = True
-    ):
-        super().__init__()
-        self._logger.add(
-            f'logs/api/' + f'{log_file}' + '_{time: YYYY-MM-DD}.log',
-            level=level,
-            rotation=rotation,
-            retention=retention,
-            compression=compression,
-            enqueue=enqueue
-        )
-
-
+@deprecated(
+    version="1.28.0",
+    reason="使用 highway_sdk.utils.logger_config 代替"
+)
 def get_driver_loger(
         name: str = 'none',
         brand: str = 'none',
