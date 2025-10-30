e915991efc54aad15797c4e737e8ea9feaf7fbce
AdzLovelace <heyinyu.sensi@foxmail.com>
2025-10-30 09:21:30 +0800

    feat(highway_sdk): 更新物联智控业务服务类以获取更多设备信息
    将 `get_code_ip_bidict` 方法重命名为 `get_devices_info`，并扩展其功能以返回更丰富的设备信息，
    包括序列号、设备ID、类别ID及系列信息。同时移除了对 `bidict` 的依赖，改为使用标准字典结构。

--- highway_sdk/platform/supaiot/business.py
+++ highway_sdk/platform/supaiot/business.py
@@ -1,28 +1,39 @@
-from typing import List
-from bidict import bidict
+from typing import Dict
 from .client import SupaiotAsyncClient
 
 
 class SupaiotBusinessService:
-    """物联智控业务服务类
-    """
+    """物联智控业务服务类"""
+
     def __init__(self, client: SupaiotAsyncClient) -> None:
         self._client = client
 
-    async def get_code_ip_bidict(self, class_id: str) -> bidict:
-        """获取物联智控中设备编码与IP的双向映射表
+    async def get_devices_info(self, class_id: str):
+        """获取设备信息
 
         Args:
-            client (SupaiotAsyncClient): _description_
-            prototype_id (str): _description_
+            class_id (str): _description_
 
         Returns:
-            bidict: _description_
+            Dict[str, dict]: {ip: {"series": xxx, "sn": xxx, "device_id": xxx, "class_id": xxx}}
         """
-        code_ip_dict = {}
+        devices_info: Dict[
+            str, dict
+        ] = {}  # {ip: {"series": xxx, "sn": xxx, "device_id": xxx, "class_id": xxx}}
+        series = None
+        res = await self._client.get_class(class_id)
+        if isinstance(res.data, dict):
+            mqtt_info = res.data.get("mqttInfo")
+            if mqtt_info:
+                for field in mqtt_info:
+                    if field["key"] == "SERIES":
+                        series = field["default"]
+
         page_num, page_size = 1, 20
         while True:
-            res = await self._client.list_devices(page_num, page_size, class_id=class_id)
+            res = await self._client.list_devices(
+                page_num, page_size, class_id=class_id
+            )
             if isinstance(res.data, dict):
                 device_list = res.data.get("data")
 
@@ -30,14 +41,22 @@ class SupaiotBusinessService:
                     break
 
                 for device in device_list:
-                    ID = device.get("ID")
-                    description: str = device.get("description", "") # 例如：ZK2+465/192.168.1.1
+                    device_id = device["ID"]
+                    description: str = device.get(
+                        "description", ""
+                    )  # 例如：ZK105+200/33.74.39.15/5009/h64w128
                     fields = description.split("/")
                     if len(fields) >= 2:
                         ip = fields[1]
-                        code_ip_dict[ID] = ip
-
+                    sn = device["mqttInfo"]["SN"]
+                    devices_info[ip] = {
+                        "series": series,
+                        "sn": sn,
+                        "device_id": device_id,
+                        "class_id": class_id,
+                    }
                 page_num += 1
             else:
                 break
-        return bidict(code_ip_dict)
+
+        return devices_info
