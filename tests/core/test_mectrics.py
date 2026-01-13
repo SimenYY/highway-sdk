e144cdfaf291b0cca23801732fbb672f79fe815c
AdzLovelace <heyinyu.sensi@foxmail.com>
2026-01-13 17:27:51 +0800

    test: 添加测试TCP连接器和Prometheus服务器的集成测试

---
+++ tests/core/test_mectrics.py
@@ -0,0 +1,37 @@
+import asyncio
+
+from highway_sdk.core.metrics import start_prometheus_server
+from highway_sdk.core.protocols import DriverTCPClientProtocol
+from highway_sdk.core.connectors import TCPReconnectingConnector
+from highway_sdk.core.log import LoguruConfig
+
+LoguruConfig.intercept_logging(["*"])
+
+LoguruConfig(name="test").set_console()
+
+
+class MyProtocol(DriverTCPClientProtocol):
+    pass
+
+
+async def main():
+    connectors = []
+    for port in [8888, 8887, 8889]:
+        connector = TCPReconnectingConnector(
+            host="127.0.0.1",
+            port=port,
+            protocol_cls=MyProtocol,
+            need_metrics=True,
+        )
+        connectors.append(connector)
+
+    async with asyncio.TaskGroup() as tg:
+        tg.create_task(start_prometheus_server())
+        await asyncio.sleep(5)
+
+        for connector in connectors:
+            tg.create_task(connector.create())
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
